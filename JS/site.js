2c274c020f44dbcb9f657dad1e374a14e7624859
Martin Vaughan <martinpaul.vaughan@yahoo.com>
2024-12-06 18:09:31 +0000

    Added mobile navigation

--- JS/site.js
+++ JS/site.js
@@ -1,9 +1,51 @@
 document.getElementById("burger-id").addEventListener("click", (e) => {
-    document.getElementById("page").style.display = "none";
-    var elements = document.getElementsByClassName("header-menu");
-    console.log("Number of elements: " + elements.length);
-    for (let i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        element.style.display = "block";
+
+    var page = document.getElementById("page");
+    var mobNav = document.getElementById("mobile-nav-id");
+
+    if (page.style.display === "none") {
+        page.style.display = "block";
+        mobNav.style.display = "none";
     }
+    else {
+        page.style.display = "none";
+        mobNav.style.display = "block";
+    }
+
+})
+
+document.getElementById("about-us-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "none";
+    document.getElementById("about-us-menu-id").style.display = "block";
+})
+
+document.getElementById("about-us-back-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "block";
+    document.getElementById("about-us-menu-id").style.display = "none";
+})
+
+document.getElementById("our-shows-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "none";
+    document.getElementById("our-shows-menu-id").style.display = "block";
+})
+
+document.getElementById("our-shows-back-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "block";
+    document.getElementById("our-shows-menu-id").style.display = "none";
+})
+
+document.getElementById("whats-on-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "none";
+    document.getElementById("whats-on-menu-id").style.display = "block";
+})
+
+document.getElementById("whats-on-back-id").addEventListener("click", (e) => {
+
+    document.getElementById("top-menu-id").style.display = "block";
+    document.getElementById("whats-on-menu-id").style.display = "none";
 })
