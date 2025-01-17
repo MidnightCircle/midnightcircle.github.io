14d8696c63f5a6c059c9a0df57d7802be0ef5345
Martin Vaughan <martinpaul.vaughan@yahoo.com>
2025-01-17 14:05:23 +0000

    Updated menu and nightmare.html

--- JS/site.js
+++ JS/site.js
@@ -1,3 +1,73 @@
+/**
+ * Dynamic What's on list
+ */
+const whatsOn = [
+    {
+        title: "A Midsummer NIGHTMARE",
+        link: "nightmare.html"
+    }
+];
+
+/**
+ * Populates the What's on menus
+ */
+function populateWhatsOn() {
+    try {
+        // Populate what's on menus
+        var deskDiv = document.getElementById("whats-on-items-desk-id");
+        var mobDiv = document.getElementById("whats-on-items-mob-id");
+
+        var deskStr = "";
+        var mobStr = "";
+
+        for (var i = 0; i < whatsOn.length; i++) {
+
+            deskStr += "<div class=\"header-nav-folder-item\">";
+            deskStr += "<a href=\"" + whatsOn[i].link + "\">";
+            deskStr += "<span class=\"header-nav-folder-item-content\">";
+            deskStr += whatsOn[i].title;
+            deskStr += "</span>";
+            deskStr += "</a>";
+            deskStr += "</div>";
+
+            mobStr += "<div class=\"container header-menu-nav-item\">";
+            mobStr += "<a href=\"" + whatsOn[i].link + "\">";
+            mobStr += "<div class=\"header-menu-nav-item-content\">";
+            mobStr += whatsOn[i].title;
+            mobStr += "</div>";
+            mobStr += "</a>";
+            mobStr += "</div>";
+                
+        }
+
+        if (deskDiv !== null) {
+            deskDiv.innerHTML = deskStr;
+        }
+        else {
+            console.error("Could not find element 'whats-on-items-desk-id'");
+        }
+
+        if (mobDiv !== null) {
+            mobDiv.innerHTML = mobStr;
+        }
+        else {
+            console.error("Could not find element 'whats-on-items-mob-id'");
+        }
+        
+    }
+    catch (err) {
+        console.error(err.message + " in populateWhatsOn()");
+    }
+}
+
+// Populate menus
+populateWhatsOn();
+
+
+/**
+ * Burger menu functionality
+ */
+
 document.getElementById("burger-id").addEventListener("click", (e) => {
 
     var page = document.getElementById("page");
@@ -49,3 +119,7 @@ document.getElementById("whats-on-back-id").addEventListener("click", (e) => {
     document.getElementById("top-menu-id").style.display = "block";
     document.getElementById("whats-on-menu-id").style.display = "none";
 })
+
+/**
+ * End burger menu functionality
+ */
